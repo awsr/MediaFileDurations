9cc336239486a08f890dd83f0ba9b18492ec1d9a
awsr <43862868+awsr@users.noreply.github.com>
2019-03-31 14:52:20 -0700

    Create durations.ps1
    Add main script.

---
+++ durations.ps1
@@ -0,0 +1,75 @@
+$FilesDir = Convert-Path "$PWD"
+$RecursiveSearch = $true # Use $true or $false
+$OutputFile = Convert-Path "$PWD\durations.txt"
+# For multiple types of files, specify as an array. (e.x. $FileExtensions = "*.wav", "*.mp3", "*.ogg")
+$FileExtensions = "*.ogg"
+
+# Use Leaf PathType to make sure we're checking for a file and not a directory.
+if (!((test-path .\ffprobe -PathType Leaf) -or (test-path .\ffprobe.exe))) {
+	Write-Error "Missing ffprobe. Must be in same directory as script." -Category ResourceUnavailable -CategoryReason "Script requires ffprobe to work."
+	Write-Host -BackgroundColor Black -ForegroundColor Green "Download ffprobe?"
+	$GetFFprobe = Read-Host "(Y)/(N)"
+	if ($GetFFprobe -like "y") {
+		Try {
+			# Use API to get latest file binaries.
+			$FFresponse = Invoke-WebRequest -DisableKeepAlive -Method Get -Uri "http://ffbinaries.com/api/v1/version/latest"
+			}
+		Catch {
+			Write-Host -BackgroundColor Black -ForegroundColor Red "ERROR: Something went wrong with getting ffprobe info."
+			Start-Sleep -s 2
+			Break
+		}
+		# Set platform.
+		if ($IsWindows) {$Platform = "windows-32"}
+		elseif ($IsLinux) {$Platform = "linux-32"}
+		elseif ($IsMacOS) {$Platform = "osx-64"}
+		else {
+			Write-Host -BackgroundColor Black -ForegroundColor Red "ERROR: Unable to determine OS platform."
+			Start-Sleep -s 2
+			Break
+		}
+		$FFbinaries = ConvertFrom-Json $FFresponse.Content
+		# Create temporary file for downloading.
+		$TempFile = New-TemporaryFile
+		Try {
+			# Download ffprobe binary.
+			Invoke-WebRequest -Uri $FFbinaries.bin.$Platform.ffprobe -OutFile $TempFile
+			}
+		Catch {
+			Write-Host -BackgroundColor Black -ForegroundColor Red "ERROR: Something went wrong with getting ffprobe binary."
+			Remove-Item $TempFile
+			Start-Sleep -s 2
+			Break
+		}
+		Expand-Archive -Path $TempFile -DestinationPath "$PWD"
+		# Remove unneeded artifact from OS X zip file creation.
+		Remove-Item -Recurse "$PWD\__MACOSX" -ErrorAction Ignore
+		# Remove temporary file.
+		Remove-Item $TempFile
+	}
+}
+
+if ((test-path .\ffprobe -PathType Leaf) -or (test-path .\ffprobe.exe)) {
+	$Output = @()
+	$TotalTime = 0.0
+	$i = 0
+	if ($RecursiveSearch) {
+		$MediaFileObjects = Get-ChildItem ($FilesDir + "\*") -Recurse -Include $FileExtensions
+	}
+	else {
+		$MediaFileObjects = Get-ChildItem ($FilesDir + "\*") -Include $FileExtensions
+	}
+
+	$MediaFileObjects | ForEach-Object -Process {
+		$i += 1
+		Write-Progress -Activity "Getting durations of $FileExtensions files." -Status "Processing file $i of $($MediaFileObjects.Length)" -PercentComplete (($i/$MediaFileObjects.Length)*100) -CurrentOperation $_.Name
+		# Works without the forced quotes (`"), but they're used just in case.
+		$Duration = .\ffprobe -v error -show_entries format=duration -of default=nk=1:nw=1 `"$_`"
+		$Output += $_.Name + " " + $Duration
+		$TotalTime += $Duration
+		}
+	$Output += "Total " + (New-TimeSpan -Seconds $TotalTime).ToString()
+	# Output to file.
+	Out-File -FilePath $OutputFile -InputObject $Output
+	Start-Sleep -m 500
+}
